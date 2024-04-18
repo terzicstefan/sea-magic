37efe1418f18de15dced1838d2253f42864aabe5
terzicstefan <terzicstefan7@gmail.com>
2024-04-18 10:47:29 +0200

    izmjene posle feedback-a

---
+++ lightbox/script.js
@@ -0,0 +1,38 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+    document.addEventListener("click", checkClickOutside);
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+    document.removeEventListener("click", checkClickOutside);
+  }
+  
+  function checkClickOutside(event) {
+    if (event.target == document.getElementById("myModal")) {
+      closeModal();
+    }
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlideG(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+   
+    slides[slideIndex-1].style.display = "block";
+  }
