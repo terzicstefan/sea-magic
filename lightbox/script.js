23191515ea20a796f1ee13ce6df9584767beacc8
terzicstefan <terzicstefan7@gmail.com>
2024-04-19 09:40:09 +0200

    izmjene posle feedback-a

--- lightbox/script.js
+++ lightbox/script.js
@@ -1,38 +1,42 @@
 function openModal() {
-    document.getElementById("myModal").style.display = "block";
-    document.addEventListener("click", checkClickOutside);
+  document.getElementById("myModal").style.display = "block";
+  document.addEventListener("click", checkClickOutside);
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+  document.removeEventListener("click", checkClickOutside);
+}
+
+function checkClickOutside(event) {
+  if (event.target == document.getElementById("myModal")) {
+    closeModal();
   }
-  
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-    document.removeEventListener("click", checkClickOutside);
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlideG(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-  
-  function checkClickOutside(event) {
-    if (event.target == document.getElementById("myModal")) {
-      closeModal();
-    }
+  if (n < 1) {
+    slideIndex = slides.length;
   }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-  function currentSlideG(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-   
-    slides[slideIndex-1].style.display = "block";
-  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
